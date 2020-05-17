43cb5b9bf7a37dbb0da2b6fb2e77b66ba43d7597
YashMunjal <yashmu45@gmail.com>
2020-05-17 20:18:19 +0530

    merge two arrays

--- C/Merge-Two-Arrays.c
+++ C/Merge-Two-Arrays.c
@@ -1,30 +1,42 @@
-int * mergeArrays(int a[], int b[], int asize, int bsize) 
+/* =========================================================================================
+
+
+Write a function mergeArrays which should merge two sorted arrays to create one single sorted array.
+
+
+======================================================*/
+
+
+
+int *mergeArrays(int a[], int b[], int asize, int bsize)
 {
-  int arr[100],i,l=(asize+bsize);
-  for(i=0;i<asize;i++)
-  {
-    arr[i]=a[i];
-  }
-  int j;
-    j=(asize);
-  for(i=0;i<bsize&&j<l;i++)
-  {
-    arr[j]=b[i];
-    j++;
-  }
-  int k;
-  for(i=0;i<l;i++)
-    for(k=i+1;k<l;k++)
+    int arr[100], i, l = (asize + bsize);
+    for (i = 0; i < asize; i++)
     {
-      if(arr[i]>arr[k])
-      {int t;
-      t=arr[i];
-      arr[i]=arr[k];
-      arr[k]=t;}
+        arr[i] = a[i];
     }
-  for(i=0;i<l;i++)
-  {
-    a[i]=arr[i];
-  }
-  return a;
+    int j;
+    j = (asize);
+    for (i = 0; i < bsize && j < l; i++)
+    {
+        arr[j] = b[i];
+        j++;
+    }
+    int k;
+    for (i = 0; i < l; i++)
+        for (k = i + 1; k < l; k++)
+        {
+            if (arr[i] > arr[k])
+            {
+                int t;
+                t = arr[i];
+                arr[i] = arr[k];
+                arr[k] = t;
+            }
+        }
+    for (i = 0; i < l; i++)
+    {
+        a[i] = arr[i];
+    }
+    return a;
 }
