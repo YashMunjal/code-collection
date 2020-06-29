9c62744627623180b4c70a4c6fff0c5068ca2962
yashmunjal <yashmu45@gmail.com>
2020-06-29 14:14:57 +0530

    count

---
+++ CodeWars-Katas/highest-lowest.js
@@ -0,0 +1,16 @@
+/*
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+Example:
+
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+
+*/
+
+function highAndLow(numbers){
+    var x = numbers.split(" ").sort((a,b)=>a-b);
+       return x[x.length-1] +" "+ x[0];
+  }
+  
